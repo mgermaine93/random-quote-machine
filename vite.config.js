6682a973dad47f6edaa95606e84c3eb54013d606
mgermaine93 <mgermaine93@gmail.com>
2026-03-13 13:33:18 -0400

    Updated vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fcc-code-challenges/tree/master/front-end-development-libraries/random-quote-machine/',
+  base: '/random-quote-machine/',
   plugins: [react()],
 })
